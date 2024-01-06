cf87fabab8a8d2a5401d5146f430edceca6af000
Charltoon <johncharltonp@gmail.com>
2024-01-07 05:31:56 +0800

    addtocart then remove from the cart

--- Environment/django_env/django_project-main/ms18/urls.py
+++ Environment/django_env/django_project-main/ms18/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('product/new/', ProductCreateView.as_view(), name='product-create'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product-update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
-    path('purchase-order/', views.purchase_order, name='purchase-order'),
     path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
-    path('cart/', views.cart_view, name='cart'),
+    path('cart/', views.cart, name='cart'),
+    path('remove/<int:cart_id>/', views.remove_from_cart, name='remove-from-cart'),
+   #path('clear-cart/', views.clear_cart, name='clear-cart'),
+   #path('about/cart/', views.cart_view, name='about-cart'),  # Add a URL pattern for the about/cart page
     path('about/', views.about, name='ms18-about'),
 ]
--- Environment/django_env/django_project-main/ms18/views.py
+++ Environment/django_env/django_project-main/ms18/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
+from django.http import HttpResponseBadRequest
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Product, PurchaseOrder
+from .models import Product, PurchaseOrder, Cart
+from django.contrib import messages
+from django.utils import timezone
 
 
 
@@ -16,7 +19,7 @@ class ProductListView(LoginRequiredMixin, ListView):
     model = Product
     template_name = 'ms18/home.html'
     context_object_name = 'products'
-    ordering = ['-date_posted']
+    ordering = ['-PROD_DATE_POSTED']
     
     def form_valid(self, form):
         form.instance.employee = self.request.user
@@ -29,32 +32,22 @@ class ProductDetailView(DetailView):
     
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
-    fields = ['title', 'content', 'image']
+    fields = ['PROD_NAME', 'PROD_DESCRIPTION', 'PROD_IMAGE', 'PROD_QUANTITY', 'PROD_PRICE']
     
     def form_valid(self, form):
         form.instance.employee = self.request.user
         return super().form_valid(form)
     
-    def test_func(self):
-        product = self.get_object()
-        if self.request.user == product.employee:
-            return True
-        return False
 
     
 class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Product
-    fields = ['title', 'content', 'image']
+    fields = ['PROD_NAME', 'PROD_DESCRIPTION', 'PROD_IMAGE']
     
     def form_valid(self, form):
         form.instance.employee = self.request.user
         return super().form_valid(form)
     
-    def test_func(self):
-        product = self.get_object()
-        if self.request.user == product.employee:
-            return True
-        return False
 
 class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Product
@@ -66,51 +59,62 @@ class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
+
 def about(request):
      # Retrieve products ordered by date_posted in descending order (newest first)
-    products = Product.objects.order_by('-date_posted')
-
+    products = Product.objects.order_by('-PROD_DATE_POSTED')
     return render(request, 'ms18/about.html', {'products': products})
-
-
-def purchase_order(request):
-   if request.method == 'POST':
-        for key, value in request.POST.items():
-            if key.startswith('quantity_'):
-                product_id = key.split('_')[1] 
-                quantity = value
-                product = get_object_or_404(Product, pk=product_id)  # Fetch the product
-
-                PurchaseOrder.objects.create(
-                    employee=request.user.username,
-                    quantity=quantity,
-                    title=product.title,  # Assuming 'title' is a field in PurchaseOrder
-                    date_posted=product.date_posted,  # Assuming 'date_posted' is a field in PurchaseOrder
-                    # Assign other relevant fields from the product to PurchaseOrder
-                    # content=product.content,
-                )
-                return HttpResponseRedirect('/success/')
-        return render(request, 'ms18/about.html')
+    
     
 def add_to_cart(request):
-    if request.method == 'POST':
-        cart = request.session.get('cart', {})  # Get the cart from the session or create a new empty cart
+    if request.user.is_authenticated and request.method == 'POST':
+        selected_products = []
         for key, value in request.POST.items():
-            if key.startswith('quantity_'):
+            if key.startswith('quantity_') and int(value) > 0:
                 product_id = key.split('_')[1]
-                quantity = int(value)
-                if quantity > 0:
-                    product = get_object_or_404(Product, id=product_id)
-                    cart_item = {
-                        'title': product.title,
-                        'quantity': quantity,
-                        # Add other product details to the cart if needed
-                    }
-                    cart[product_id] = cart_item
-        request.session['cart'] = cart  # Update the cart in the session
-        return HttpResponseRedirect('/cart/')  # Redirect to the cart page
-    return render(request, 'ms18/about.html')  # Render the same page if not a POST request
+                try:
+                    product = Product.objects.get(pk=product_id)
+                    selected_products.append(product)
+                    # Create a PurchaseOrder for the selected product
+                    PurchaseOrder.objects.create(
+                        ORD_EMPLOYEE=request.user.username,
+                        ORD_DATE_POSTED=timezone.now(),
+                        ORD_NAME=product.PROD_NAME,
+                        ORD_QUANTITY=int(value),
+                        ORD_DESCRIPTION=product.PROD_DESCRIPTION
+                    )
+                except Product.DoesNotExist:
+                    messages.error(request, f"Product with ID {product_id} does not exist.")
+                except Exception as e:
+                    messages.error(request, f"An error occurred while adding product with ID {product_id}: {e}")
+        
+        if selected_products:
+            messages.success(request, 'Items added to cart successfully!')
+        
+        return redirect('cart')  # Redirect to a 'cart' view or another appropriate view
+    else:
+        messages.error(request, 'Please log in to add items to the cart.')
+        return redirect('login')  # Redirect to the login page if the user is not authenticated
+ 
+    
+def cart(request):
+    # Retrieve items in the cart based on the logged-in user
+    user = request.user
+    cart_items = PurchaseOrder.objects.filter(ORD_EMPLOYEE=user.username)
 
-def cart_view(request):
-    cart = request.session.get('cart', {})  # Retrieve cart information from the session
-    return render(request, 'ms18/cart.html', {'cart': cart})
+    # Print out cart_items for inspection
+    print(cart_items)
+
+    context = {
+        'cart_items': cart_items
+    }
+    return render(request, 'ms18/cart.html', context)
+
+def remove_from_cart(request, cart_id):
+    if request.method == 'POST':
+        cart_item = get_object_or_404(PurchaseOrder, pk=cart_id)
+        cart_item.delete()
+        messages.success(request, "Item removed from cart!")
+        return redirect('cart')
+    else:
+        return HttpResponseBadRequest("Invalid request method")
